7b38f5f6b85e27b8d7b14cb65c89f8779df7a193
AndreIglesias <>
2023-08-18 09:24:39 +0200

    tmp

--- src/train.py
+++ src/train.py
@@ -15,8 +15,8 @@ def normalize(milage: np.ndarray, pos) -> float:
 def get_theta(theta, milage: np.ndarray, price, pos) -> list:
     if pos > 0:
         theta = get_theta(theta, milage, price, pos - 1)
-    theta[0] += estimate_price(theta, normalize(milage, pos)) - price[pos]
-    theta[1] += (estimate_price(theta, normalize(milage, pos)) - price[pos]) * normalize(milage, pos)
+    theta[0] += estimate_price([0,0], normalize(milage, pos)) - price[pos]
+    theta[1] += (estimate_price([0,0], normalize(milage, pos)) - price[pos]) * normalize(milage, pos)
 
     # for i in range(milage.size):
     #     theta[0] += estimate_price(theta, normalize(milage, i)) - price[i]
@@ -27,7 +27,6 @@ if __name__=="__main__":
     milage, price = [],[]
     size = 0
     theta = [0,0]
-    tmp = [0,0]
     with open('data.csv', newline='') as csvfile:
         spamreader = csv.DictReader(csvfile)
         spamreader = sorted(spamreader, key=lambda x: float(x['km']))
@@ -38,11 +37,11 @@ if __name__=="__main__":
             # tmp[1] += (estimate_price(theta, milage[size]) - price[size]) * milage[size]
             size += 1
     # theta[1], theta[0] =  np.polyfit(milage, price, 1)
-    # tmp = get_theta(tmp, np.array(milage), np.array(price), size - 1)
-    # theta[0] -= lrate * tmp[0] / size
-    # theta[1] -= lrate * tmp[1] / size
-    theta[1] = (size * sum([milage[i] * price[i] for i in range(size)]) - (sum(milage) * sum(price))) / (size * sum([milage[i] * milage[i] for i in range(size)]) - (sum(milage) * sum(milage)))
-    theta[0] = (sum(price) - theta[1] * sum(milage)) / size
+    tmp = get_theta([0, 0], np.array(milage), np.array(price), size - 1)
+    theta[0] -= lrate * tmp[0] / size
+    theta[1] -= lrate * tmp[1] / size
+    # theta[1] = (size * sum([milage[i] * price[i] for i in range(size)]) - (sum(milage) * sum(price))) / (size * sum([milage[i] * milage[i] for i in range(size)]) - (sum(milage) * sum(milage)))
+    # theta[0] = (sum(price) - theta[1] * sum(milage)) / size
     print(theta)
     pyplot.scatter(milage, price)
     pyplot.plot(milage, [theta[0] + theta[1] * x for x in milage])
