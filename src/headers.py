17057ccdb57d730c1862cc10ae6c8d442dc6c44f
Juan Carlos Iglesias <jiglesia@student.42.fr>
2023-08-18 16:15:26 +0200

    all normalized

--- src/headers.py
+++ src/headers.py
@@ -1,6 +1,16 @@
+import numpy as np
+
 thetafile = "theta.npy"
 lrate = 0.1
 epoch = 1000
 
 def estimate_price(theta, milage) -> float:
     return theta[0] + theta[1] * milage
+
+def normalize(milage: np.ndarray) -> np.ndarray:
+    return (milage - milage.min()) / (milage.max()- milage.min())
+
+def normalize_price(price: np.ndarray, milage: np.ndarray):
+    m = (milage.size * np.sum(milage * price) - (np.sum(milage) * np.sum(price))) / (milage.size * np.sum(milage * milage) - (np.sum(milage) * np.sum(milage)))
+    b = (np.sum(price) - m * np.sum(milage)) / milage.size
+    return estimate_price([b, m], normalize(milage))
--- src/train.py
+++ src/train.py
@@ -8,21 +8,14 @@ from matplotlib import pyplot
 import csv
 import numpy as np
 
-
-def normalize(milage: np.ndarray) -> np.ndarray:
-    return (milage - milage.min()) / (milage.max()- milage.min())
-
-# def norm_ret(arg: np.ndarray):
-#     return arg * (arg.max() - arg.min()) + arg.min()
-
 def pred_error(theta, milage, price) -> float:
     return estimate_price(theta, milage) - price
 
 def get_theta(theta, milage: np.ndarray, price: np.ndarray, size: int) -> list:
     tmp = theta
-    tmp[0] -= lrate * np.sum(pred_error(theta, milage, price)) / size
-    tmp[1] -= lrate * np.sum(pred_error(theta, milage, price) * milage) / size
-    # print(tmp)
+    for i in range(size):
+        tmp[0] -= lrate * np.sum(pred_error(theta, milage[:i + 1], price[:i + 1])) / size
+        tmp[1] -= lrate * np.sum(pred_error(theta, milage[:i + 1], price[:i + 1]) * milage[:i + 1]) / size
     return tmp
 
 if __name__=="__main__":
@@ -30,21 +23,14 @@ if __name__=="__main__":
     theta = [0,0]
     with open('data.csv', newline='') as csvfile:
         spamreader = csv.DictReader(csvfile)
-        # spamreader = sorted(spamreader, key=lambda x: float(x['km']))
         for row in spamreader:
             milage.append(int(row["km"]))
             price.append(int(row["price"]))
     milage = np.array(milage)
     price = np.array(price)
-    theta[1] = (milage.size * np.sum(milage * price) - (np.sum(milage) * np.sum(price))) / (milage.size * np.sum(milage * milage) - (np.sum(milage) * np.sum(milage)))
-    theta[0] = (np.sum(price) - theta[1] * np.sum(milage)) / milage.size
-    tmp = estimate_price(theta, normalize(milage))
-    # theta = [0,0]
     for _ in range(epoch):
-        theta = get_theta(theta, normalize(milage), tmp, milage.size)
-    # theta[1] = (np.sum(price) - (theta[0] * milage.size)) / np.sum(milage)
-    print(f"{theta[0]:.2f}, {theta[1]:.2f}")
-    # pyplot.scatter(milage, price)
-    # pyplot.plot(milage, estimate_price(theta, milage))
-    # pyplot.show()
+        theta = get_theta(theta, normalize(milage), normalize_price(price, milage), milage.size)
+    pyplot.scatter(milage, price)
+    pyplot.plot(milage, estimate_price(theta, milage), color="green")
+    pyplot.show()
     np.save(thetafile, theta)
